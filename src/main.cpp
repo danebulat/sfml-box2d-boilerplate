0ed46ecfb7111577d8d0e9e02a18512d246fd9c7
Dane Bulat <dane.bulat@gmail.com>
2020-09-22 16:57:02 +0900

    basic edge chain added in world

--- src/main.cpp
+++ src/main.cpp
@@ -42,7 +42,49 @@ int main(int argc, char** argv)
 	/** Prepare the world */
 	b2Vec2 gravity(0.f, 9.8f);
 	b2World world(gravity);
-	CreateGround(world, 400.f, 500.f);
+	CreateGround(world, 400.f, 575.f);
+
+	/* Create edge chain */
+	std::vector<sf::Vector2f> chainCoords =  {
+		sf::Vector2f(20.f,  540.f),
+		sf::Vector2f(150.f, 470.f),
+		sf::Vector2f(360.f, 507.f),
+		sf::Vector2f(617.f, 435.f),
+		sf::Vector2f(750.f, 540.f)
+	};
+
+	sf::VertexArray chainVa(sf::LinesStrip, 5);
+	chainVa[0].position = chainCoords[0];
+	chainVa[1].position = chainCoords[1];
+	chainVa[2].position = chainCoords[2];
+	chainVa[3].position = chainCoords[3];
+	chainVa[4].position = chainCoords[4];
+
+	for (std::size_t i = 0; i < 5; ++i)
+		chainVa[i].color = sf::Color::Blue;
+
+	b2Vec2 vs[5];
+	vs[0].Set(chainCoords[0].x / SCALE, chainCoords[0].y / SCALE);
+	vs[1].Set(chainCoords[1].x / SCALE, chainCoords[1].y /SCALE);
+	vs[2].Set(chainCoords[2].x / SCALE, chainCoords[2].y /SCALE);
+	vs[3].Set(chainCoords[3].x / SCALE, chainCoords[3].y /SCALE);
+	vs[4].Set(chainCoords[4].x / SCALE, chainCoords[4].y /SCALE);
+
+	b2ChainShape chain;
+	chain.CreateChain(vs, 5,
+		b2Vec2(chainCoords[0].x - 10.f / SCALE, chainCoords[0].y / SCALE),
+		b2Vec2(chainCoords[4].x + 10.f / SCALE, chainCoords[4].y / SCALE));
+
+	b2FixtureDef f;
+	f.density = 0.f;
+	f.shape = &chain;
+
+	b2BodyDef d;
+	d.position.Set(0,0);
+	d.type = b2_staticBody;
+	b2Body* b = world.CreateBody(&d);
+
+	b->CreateFixture(&f);
 
 	while (window.isOpen())
 	{
@@ -214,6 +256,7 @@ int main(int argc, char** argv)
 			window.draw(line);
 
 		window.draw(mouseLabel);
+		window.draw(chainVa);
 
 		window.display();
 	}
