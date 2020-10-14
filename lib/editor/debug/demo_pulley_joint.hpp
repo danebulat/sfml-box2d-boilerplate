f837a0636fe17f808f414970577fdbcfa5829804
Dane Bulat <dane.bulat@gmail.com>
2020-10-14 15:05:08 +0900

    lines rendered in pulley joint demo

--- lib/editor/debug/demo_pulley_joint.hpp
+++ lib/editor/debug/demo_pulley_joint.hpp
@@ -6,6 +6,11 @@
 #include "editor/constants.hpp"
 #include "editor/debug/demo_structs.hpp"
 
+/** DemoPulleyJoint class
+ *
+ * TODO: LMB to apply force and torque.
+ */
+
 class DemoPulleyJoint
 {
 private:
@@ -15,6 +20,12 @@ private:
 	BoxSprite		m_box;
 	CircleSprite	m_circle;
 
+	sf::VertexArray	m_line1;
+	sf::VertexArray	m_line2;
+
+	sf::Vector2f	m_groundAnchor1;
+	sf::Vector2f	m_groundAnchor2;
+
 public:
 	DemoPulleyJoint(b2World* world)
 	{
@@ -23,7 +34,7 @@ public:
 
 		// body definition
 		b2BodyDef bodyDef;
-		bodyDef.position = b2Vec2(200.f/SCALE, 280.f/SCALE);
+		bodyDef.position = b2Vec2(350.f/SCALE, 300.f/SCALE);
 		bodyDef.type = b2_dynamicBody;
 		bodyDef.angularDamping = .1;
 
@@ -48,8 +59,8 @@ public:
 
 		// BodyB (circle)
 		fixtureDef.shape = &circleShape;
-		fixtureDef.density = 2.f;
-		bodyDef.position.Set(230.f/SCALE, 280.f/SCALE);
+		fixtureDef.density = 1.f;
+		bodyDef.position.Set(400.f/SCALE, 280.f/SCALE);
 		b2Body* bodyB = m_world->CreateBody(&bodyDef);
 		bodyB->CreateFixture(&fixtureDef);
 
@@ -57,8 +68,9 @@ public:
 		b2Vec2 anchor1 = bodyA->GetWorldCenter();
 		b2Vec2 anchor2 = bodyB->GetWorldCenter();
 
-		b2Vec2 groundAnchor1(bodyA->GetWorldCenter().x,
-							 bodyA->GetWorldCenter().y - (50.f/SCALE));
+		b2Vec2 groundAnchor1(
+			bodyA->GetWorldCenter().x,
+			bodyA->GetWorldCenter().y - (50.f/SCALE));
 
 		b2Vec2 groundAnchor2(bodyB->GetWorldCenter().x,
 							 bodyB->GetWorldCenter().y - (50.f/SCALE));
@@ -72,7 +84,32 @@ public:
 
 		m_joint = m_world->CreateJoint(&jointDef);
 
-		// TODO: Lines representing pulley
+		// Ground anchor data members
+		m_groundAnchor1.x = groundAnchor1.x * SCALE;
+		m_groundAnchor1.y = groundAnchor1.y * SCALE;
+
+		m_groundAnchor2.x = groundAnchor2.x * SCALE;
+		m_groundAnchor2.y = groundAnchor2.y * SCALE;
+
+		// Line position
+		m_line1.resize(2);
+		m_line1.setPrimitiveType(sf::LineStrip);
+
+		m_line1[0].position = m_groundAnchor1;
+		m_line1[1].position = m_box.m_position;
+
+		m_line2.resize(2);
+		m_line2.setPrimitiveType(sf::LineStrip);
+
+		m_line2[0].position = m_groundAnchor2;
+		m_line2[1].position = m_circle.m_position;
+
+		// Line color
+		m_line1[0].color = sf::Color::Blue;
+		m_line1[1].color = sf::Color::Blue;
+
+		m_line2[0].color = sf::Color::Blue;
+		m_line2[1].color = sf::Color::Blue;
 	}
 
 	~DemoPulleyJoint()
@@ -93,12 +130,21 @@ public:
 		b2Vec2 bPos = m_joint->GetBodyB()->GetWorldCenter();
 		m_circle.SetPosition(sf::Vector2f(bPos.x*SCALE, bPos.y*SCALE));
 		m_circle.m_sprite.setRotation(m_joint->GetBodyB()->GetAngle()*180/b2_pi);
+
+		m_line1[1].position.x = aPos.x*SCALE;
+		m_line1[1].position.y = aPos.y*SCALE;
+
+		m_line2[1].position.x = bPos.x*SCALE;
+		m_line2[1].position.y = bPos.y*SCALE;
 	}
 
 	void Draw(sf::RenderWindow& window)
 	{
 		window.draw(m_box.m_sprite);
 		window.draw(m_circle.m_sprite);
+
+		window.draw(m_line1);
+		window.draw(m_line2);
 	}
 };
 
